320526fc1331bf5fad8c7357ec4507a1a7afad12
Predrag Buncic <pbuncic@yahoo.com>
2025-10-28 20:13:36 +0100

    Using include tag to share overrides between defaults

--- defaults-dev3.sh
+++ defaults-dev3.sh
@@ -1,5 +1,6 @@
 package: defaults-dev3
 version: v1
+<<<<<<< HEAD
 disable:
   - MySQL
 overrides:
@@ -12,3 +13,11 @@ overrides:
   Gaudi:
     tag: 'master'
 ---
+=======
+env:
+  MACOSX_DEPLOYMENT_TARGET: '14.0'
+
+overrides: !include dev3.yaml
+
+ ---
+>>>>>>> e74fa35 (Using include tag to share overrides between defaults)
