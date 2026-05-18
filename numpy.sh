9cdab41054eb72c3c72cf8662556ec8929679400
Predrag Buncic <pbuncic@yahoo.com>
2026-05-19 01:01:06 +0200

    Updating version

--- numpy.sh
+++ numpy.sh
@@ -1,9 +1,9 @@
 package: numpy
 description: NumPy fundamental package for array computing in Python
-version: "2.4.0"
-tag: "2.4.0"
+version: "2.4.4"
+tag: "2.4.4"
 sources:
-  - https://lcgpackages.web.cern.ch/tarFiles/sources/numpy-2.4.0.tar.gz
+  - https://lcgpackages.web.cern.ch/tarFiles/sources/numpy-2.4.4.tar.gz
 requires:
   - Python
   - setuptools
@@ -26,27 +26,6 @@ MODULE_OPTIONS="--bin --python"
 
 function Prepare() {
   rsync -av --delete --exclude '**/.git' --delete-excluded "$SOURCEDIR"/ ./
-  # numpy 2.4.0 bundles a Highway version whose float16_t increment, decrement,
-  # and compound-assignment operators are incorrectly marked 'const' while
-  # modifying the 'native' data member.  GCC 14+ correctly rejects this.
-  # Remove the erroneous 'const' qualifiers from those specific operators.
-  "${PYTHON_EXE}" -c "
-import re, sys
-f = 'numpy/_core/src/highway/hwy/base.h'
-try:
-    t = open(f).read()
-except FileNotFoundError:
-    print('hwy/base.h not found, skipping patch', file=sys.stderr)
-    sys.exit(0)
-# operator++/-- returning float16_t or float16_t& that mutate 'native'
-t = re.sub(r'(constexpr float16_t[& ]+operator[+\-]{1,2}\([^)]*\))\s+const\b',
-           r'\1', t)
-# HWY_FLOAT16_BINARY_OP macro: assign_name variants return float16_t& and mutate
-t = re.sub(r'(float16_t&\s+assign_name\([^)]*\))\s+const\b',
-           r'\1', t)
-open(f, 'w').write(t)
-print('Patched hwy/base.h: removed const from float16_t mutating operators')
-" >&2
 }
 
 function MakeInstall() {
