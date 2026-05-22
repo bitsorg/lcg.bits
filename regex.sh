e6d1fe2242ca0f34fa02f4318eed1fa8eb3a8632
Predrag Buncic <pbuncic@yahoo.com>
2026-05-22 23:37:18 +0200

    Adding missing package

---
+++ regex.sh
@@ -0,0 +1,21 @@
+package: regex
+description: Alternative Python regular expression module with advanced features
+version: "2023.5.5"
+tag: "2023.5.5"
+sources:
+  - https://lcgpackages.web.cern.ch/tarFiles/sources/regex-2023.5.5.tar.gz
+requires:
+  - Python
+  - setuptools
+  - pip
+build_requires:
+  - bits-recipe-tools
+  - "GCC-Toolchain:(?!osx)"
+license: Apache-2.0
+---
+#!/bin/bash -e
+##############################
+. $(bits-include PythonRecipe)
+##############################
+MODULE_OPTIONS="--python"
+##############################
