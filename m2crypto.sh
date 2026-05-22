240e104ccbd87cef1252f81263c70c6cd332874b
Predrag Buncic <pbuncic@yahoo.com>
2026-05-22 21:08:52 +0000

    Add m2crypto.sh: Python/OpenSSL wrapper, version 0.45.1
    PythonRecipe with MakeInstall() override to export OPENSSL_PATH
    and SWIG_LIB env vars that setup.py reads for locating OpenSSL
    headers/libs and the SWIG share directory before pip runs.
    Required by pytools.sh.

---
+++ m2crypto.sh
@@ -0,0 +1,35 @@
+package: m2crypto
+description: M2Crypto Python wrapper for OpenSSL providing crypto and SSL toolkit
+version: "0.45.1"
+tag: "0.45.1"
+sources:
+  - https://lcgpackages.web.cern.ch/tarFiles/sources/m2crypto-0.45.1.tar.gz
+requires:
+  - Python
+  - setuptools
+  - pip
+  - swig
+  - OpenSSL
+build_requires:
+  - bits-recipe-tools
+  - "GCC-Toolchain:(?!osx)"
+license: BSD-2-Clause
+---
+#!/bin/bash -e
+##############################
+. $(bits-include PythonRecipe)
+##############################
+MODULE_OPTIONS="--python"
+##############################
+function MakeInstall() {
+  mkdir -p "${SITE_PACKAGES}"
+  # setup.py reads OPENSSL_PATH and SWIG_LIB from the environment
+  [[ -n "$OPENSSL_ROOT" ]] && export OPENSSL_PATH="$OPENSSL_ROOT"
+  if [[ -n "$SWIG_ROOT" ]]; then
+    _swig_ver=$(swig -version 2>&1 | awk '/SWIG Version/{print $3}')
+    export SWIG_LIB="$SWIG_ROOT/share/swig/$_swig_ver"
+  fi
+  "${PYTHON_EXE}" -m pip install \
+    --no-deps --no-build-isolation --ignore-installed \
+    --root=/ --prefix="${INSTALLROOT}" .
+}
