7f3457634cfbdac4dec93a6217854e45599ae15e
Predrag Buncic <pbuncic@yahoo.com>
2026-05-22 23:24:54 +0200

    Adding missing package

---
+++ huggingface_hub.sh
@@ -0,0 +1,26 @@
+package: huggingface_hub
+description: Hugging Face Hub client library for model/dataset download and sharing
+version: "0.35.1"
+tag: "0.35.1"
+sources:
+  - https://lcgpackages.web.cern.ch/tarFiles/sources/huggingface_hub-0.35.1.tar.gz
+requires:
+  - Python
+  - setuptools
+  - pip
+  - requests
+  - filelock
+  - tqdm
+  - PyYAML
+  - packaging
+build_requires:
+  - bits-recipe-tools
+  - "GCC-Toolchain:(?!osx)"
+license: Apache-2.0
+---
+#!/bin/bash -e
+##############################
+. $(bits-include PythonRecipe)
+##############################
+MODULE_OPTIONS="--bin --python"
+##############################
