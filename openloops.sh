38037412c6a7693e59ba0538df76d3bcc59f3575
Predrag Buncic <pbuncic@yahoo.com>
2026-05-24 00:59:32 +0200

    Bulk change to PythonPipRecipe

--- openloops.sh
+++ openloops.sh
@@ -5,7 +5,6 @@ tag: "2.1.2"
 sources:
   - https://lcgpackages.web.cern.ch/tarFiles/sources/MCGeneratorsTarFiles/OpenLoops-2.1.2.tar.gz
 requires:
-  - CMake
   - Python
 build_requires:
   - bits-recipe-tools
@@ -14,16 +13,23 @@ license: GPL-3.0-or-later
 ---
 #!/bin/bash -e
 ##############################
-. $(bits-include CMakeRecipe)
+. $(bits-include MakeRecipe)
 ##############################
 MODULE_OPTIONS="--bin --lib"
 ##############################
+function Prepare() {
+  rsync -av --delete --exclude '**/.git' --delete-excluded "${SOURCEDIR}"/ ./
+}
+
 function Make() {
-  ./openloops libinstall  --jobs=5 ${openloops_public_proc} compile_extra=1  \
-  && bash -c "sed -i 's/^process_repositories.*/process_repositories = matrix,powheg,ATLAS/' $SOURCEDIR/pyol/config/default.cfg" \
-  && ./openloops libinstall --jobs=5 all.coll compile_extra=1 \
-  && bash -c "sed -i 's/^process_repositories.*/process_repositories = matrix,powheg,ATLAS,public/' $SOURCEDIR/pyol/config/default.cfg" \
-  && ./openloops libinstall --jobs=5 pphtt_nf compile_extra=1 \
-  && cmake -E remove_directory $SOURCEDIR/process_obj \
-  && cmake -E remove_directory $SOURCEDIR/process_src
+  ./openloops libinstall --jobs=${JOBS:-5} ${openloops_public_proc} compile_extra=1
+  sed -i 's/^process_repositories.*/process_repositories = matrix,powheg,ATLAS/' pyol/config/default.cfg
+  ./openloops libinstall --jobs=${JOBS:-5} all.coll compile_extra=1
+  sed -i 's/^process_repositories.*/process_repositories = matrix,powheg,ATLAS,public/' pyol/config/default.cfg
+  ./openloops libinstall --jobs=${JOBS:-5} pphtt_nf compile_extra=1
+  rm -rf process_obj process_src
+}
+
+function MakeInstall() {
+  cp -a . "$INSTALLROOT/"
 }
