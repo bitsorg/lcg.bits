a5044efe2eadf21582d0058ff58a9153a018b8f3
Predrag Buncic <pbuncic@yahoo.com>
2026-05-23 22:06:53 +0200

    Fixing recipe

--- iso8601.sh
+++ iso8601.sh
@@ -2,8 +2,6 @@ package: iso8601
 description: iso8601 simple ISO 8601 date/time parser for Python
 version: "1.0.2"
 tag: "1.0.2"
-sources:
-  - https://lcgpackages.web.cern.ch/tarFiles/sources/%(name)s-%(version)s.tar.gz
 requires:
   - Python
 build_requires:
@@ -13,7 +11,7 @@ license: MIT
 ---
 #!/bin/bash -e
 ##############################
-. $(bits-include PythonRecipe)
+. $(bits-include PythonPipRecipe)
 ##############################
 MODULE_OPTIONS="--bin --python"
 ##############################
