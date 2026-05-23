8729a9b22e55a034211f928aa57603a7ce2ac772
Predrag Buncic <pbuncic@yahoo.com>
2026-05-23 12:12:16 +0200

    Fixing recipes

--- java.sh
+++ java.sh
@@ -3,7 +3,8 @@ description: OpenJDK Java Development Kit
 version: "17.0.18p8"
 tag: "17.0.18p8"
 sources:
-  - https://lcgpackages.web.cern.ch/tarFiles/sources/openjdk-17.0.18p8-linux-${JAVA_ARCH}.tar.gz
+  - "(*x86-64*linux*)https://lcgpackages.web.cern.ch/tarFiles/sources/openjdk-%(version)s-linux-x64.tar.gz"
+  - "(*aarch64*linux*)https://lcgpackages.web.cern.ch/tarFiles/sources/openjdk-%(version)s-linux-aarch64.tar.gz"
 build_requires:
   - bits-recipe-tools
   - "GCC-Toolchain:(?!osx)"
