577ff43cc7bd541fe7d07ad7c6cdd680dc5bcf75
Predrag Buncic <pbuncic@yahoo.com>
2026-05-23 22:02:31 +0200

    Fixing workarea

--- pythia6.sh
+++ pythia6.sh
@@ -2,8 +2,6 @@ package: pythia6
 description: Pythia 6 Monte Carlo event generator (legacy Fortran version)
 version: "6.4.28.snd"
 tag: "6.4.28.snd"
-requires:
-  - CMake
 build_requires:
   - bits-recipe-tools
   - "GCC-Toolchain:(?!osx)"
@@ -11,7 +9,27 @@ license: LicenseRef-Pythia6
 ---
 #!/bin/bash -e
 ##############################
-. $(bits-include CMakeRecipe)
+. $(bits-include MakeRecipe)
 ##############################
-MODULE_OPTIONS="--bin --lib"
+MODULE_OPTIONS="--lib"
 ##############################
+function Prepare() {
+  # Single compressed Fortran file; bits cannot untar a bare .f.gz.
+  # Download and decompress manually.
+  curl -fSL \
+    "https://lcgpackages.web.cern.ch/tarFiles/sources/MCGeneratorsTarFiles/pythia-6.4.28.f.gz" \
+    | gunzip > pythia6.f
+}
+
+function Make() {
+  local fflags="-std=legacy -fallow-argument-mismatch -O2 -fPIC"
+  ${FC:-gfortran} $fflags -c pythia6.f -o pythia6.o
+  ${FC:-gfortran} $fflags -shared -o libpythia6.so pythia6.o
+  ${AR:-ar} crs libpythia6.a pythia6.o
+}
+
+function MakeInstall() {
+  install -dm755 "$INSTALLROOT/lib"
+  install -m755 libpythia6.so "$INSTALLROOT/lib/"
+  install -m644 libpythia6.a  "$INSTALLROOT/lib/"
+}
