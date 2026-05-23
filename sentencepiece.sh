1849f9ae9dec06908adee48035cd5f90a20ac955
Predrag Buncic <pbuncic@yahoo.com>
2026-05-23 21:09:52 +0200

    Fixing recipes

--- sentencepiece.sh
+++ sentencepiece.sh
@@ -2,8 +2,8 @@ package: sentencepiece
 description: SentencePiece unsupervised text tokenizer
 version: "0.2.0"
 tag: "0.2.0"
-sources:
-  - https://lcgpackages.web.cern.ch/tarFiles/sources/%(name)s-%(version)s.tar.gz
+#sources:
+#  - https://lcgpackages.web.cern.ch/tarFiles/sources/%(name)s-%(version)s.tar.gz
 requires:
   - Python
 build_requires:
@@ -15,7 +15,7 @@ patches:
 ---
 #!/bin/bash -e
 ##############################
-. $(bits-include PythonRecipe)
+. $(bits-include PythonPipRecipe)
 ##############################
 MODULE_OPTIONS="--bin --python"
 ##############################
