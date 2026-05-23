e5e2d4fd2979deb9b3779d09d5b8a0545a2ac1c4
Predrag Buncic <pbuncic@yahoo.com>
2026-05-23 22:50:53 +0200

    Fixing recipes

--- catboost.sh
+++ catboost.sh
@@ -2,8 +2,6 @@ package: catboost
 description: CatBoost gradient boosting on decision trees library
 version: "1.2"
 tag: "1.2"
-sources:
-  - https://lcgpackages.web.cern.ch/tarFiles/sources/%(name)s-%(version)s.tar.gz
 requires:
   - Python
 build_requires:
@@ -13,7 +11,7 @@ license: Apache-2.0
 ---
 #!/bin/bash -e
 ##############################
-. $(bits-include PythonRecipe)
+. $(bits-include PythonPipRecipe)
 ##############################
 MODULE_OPTIONS="--bin --python"
 ##############################
--- xdrlib3.sh
+++ xdrlib3.sh
@@ -2,8 +2,6 @@ package: xdrlib3
 description: xdrlib3 XDR data packing/unpacking for Python 3
 version: "0.1.1"
 tag: "0.1.1"
-sources:
-  - https://lcgpackages.web.cern.ch/tarFiles/sources/%(name)s-%(version)s.tar.gz
 requires:
   - Python
 build_requires:
@@ -13,7 +11,7 @@ license: Python-2.0
 ---
 #!/bin/bash -e
 ##############################
-. $(bits-include PythonRecipe)
+. $(bits-include PythonPipRecipe)
 ##############################
 MODULE_OPTIONS="--bin --python"
 ##############################
