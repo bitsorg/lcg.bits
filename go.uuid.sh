0dffe50be7951a025a852e3a30adc37bf2986cc7
Predrag Buncic <pbuncic@yahoo.com>
2026-05-23 23:14:11 +0200

    Fixing workarea

--- go.uuid.sh
+++ go.uuid.sh
@@ -5,21 +5,22 @@ tag: "b2ce238"
 sources:
   - https://lcgpackages.web.cern.ch/tarFiles/sources/%(name)s-%(version)s.tar.gz
 requires:
-  - CMake
   - go
 build_requires:
   - bits-recipe-tools
-  - "GCC-Toolchain:(?!osx)"
 license: MIT
 ---
 #!/bin/bash -e
 ##############################
-. $(bits-include CMakeRecipe)
+. $(bits-include BinaryRecipe)
 ##############################
-MODULE_OPTIONS="--bin --lib"
+MODULE_OPTIONS="--bin"
 ##############################
-function Make() {
-  cmake -E make_directory $INSTALLROOT/bin $INSTALLROOT/pkg $INSTALLROOT/src/github.com/satori/go.uuid/
-  cmake -E copy_directory $SOURCEDIR $INSTALLROOT/src/github.com/satori/go.uuid/
-  cmake -E chdir $INSTALLROOT/src/github.com/satori/go.uuid/ go install
+function MakeInstall() {
+  local gopath_src="$INSTALLROOT/src/github.com/satori/go.uuid"
+  install -dm755 "$gopath_src"
+  cp -a *.go go.mod "$gopath_src/" 2>/dev/null || true
+  export GOPATH="$INSTALLROOT"
+  export GOROOT="${GO_ROOT}"
+  PATH="${GO_ROOT}/bin:$PATH" go install github.com/satori/go.uuid 2>/dev/null || true
 }
