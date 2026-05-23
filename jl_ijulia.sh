f98d7c8a5ca6762d191aa063af9bef0c2a4e3bc9
Predrag Buncic <pbuncic@yahoo.com>
2026-05-23 17:52:47 +0200

    Fixing workarea

--- jl_ijulia.sh
+++ jl_ijulia.sh
@@ -13,9 +13,25 @@ license: MIT
 ##############################
 . $(bits-include MakeRecipe)
 ##############################
-MODULE_OPTIONS="--bin --lib"
+MODULE_OPTIONS=""
 ##############################
 function Make() {
-  make ${JOBS:+-j $JOBS}
-  julia $BUILDDIR/install-packages.jl
+  # No source to compile.  Install the IJulia package into INSTALLROOT,
+  # which acts as a Julia depot (registries/, packages/, artifacts/, ...).
+  export JULIA_DEPOT_PATH="$INSTALLROOT"
+  julia -e '
+    import Pkg
+    Pkg.add(Pkg.PackageSpec(name="IJulia", version="1.29.0"))
+    Pkg.precompile()
+  '
+}
+
+function MakeInstall() {
+  : # Julia wrote directly to INSTALLROOT during Make(); nothing to install.
+}
+
+function PostInstall() {
+  # Append a JULIA_DEPOT_PATH prepend to the generated modulefile so that
+  # Julia finds this depot at runtime.  PostInstall runs after MakeModule.
+  echo 'prepend-path JULIA_DEPOT_PATH $PREFIX' >> "$MODULEFILE"
 }
