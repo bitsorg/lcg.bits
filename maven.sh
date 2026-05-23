3483b89ce3ed4202cf9caa0f37ff1654103eeb35
Predrag Buncic <pbuncic@yahoo.com>
2026-05-23 18:09:18 +0200

    Fixing workarea

--- maven.sh
+++ maven.sh
@@ -10,8 +10,6 @@ build_requires:
   - bits-recipe-tools
   - "GCC-Toolchain:(?!osx)"
 license: Apache-2.0
-patches:
-  - maven-3.8.5.patch
 ---
 #!/bin/bash -e
 ##############################
@@ -19,3 +17,15 @@ patches:
 ##############################
 MODULE_OPTIONS="--bin"
 ##############################
+function Configure() {
+  # conf/settings.xml is distributed with CRLF line endings; normalise first
+  # so that sed patterns match cleanly.
+  sed -i 's/\r//' conf/settings.xml
+  # Comment out the maven-default-http-blocker mirror added in Maven 3.8.1+.
+  # It blocks all plain-HTTP repositories (including CERN's), which breaks
+  # builds in the LCG environment.  Two <mirror> tags exist in the file:
+  # the first is inside a <!-- --> doc-comment; the second (/2) is the live
+  # HTTP-blocker block we wrap in a comment here.
+  sed -i 's|^    <mirror>$|    <!-- <mirror>|2' conf/settings.xml
+  sed -i '/<blocked>true<\/blocked>/{n; s|^    </mirror>$|    </mirror>\n    -->|;}' conf/settings.xml
+}
