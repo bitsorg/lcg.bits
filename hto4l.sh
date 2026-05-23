bb2a851cb8500f89e9a7e69869f70821859bbcbb
Predrag Buncic <pbuncic@yahoo.com>
2026-05-23 23:29:38 +0200

    Fixing workarea

--- hto4l.sh
+++ hto4l.sh
@@ -5,7 +5,6 @@ tag: "2.02"
 sources:
   - https://lcgpackages.web.cern.ch/tarFiles/sources/MCGeneratorsTarFiles/Hto4l-v2.02.tar.bz2
 requires:
-  - CMake
   - GSL
 build_requires:
   - bits-recipe-tools
@@ -16,12 +15,16 @@ patches:
 ---
 #!/bin/bash -e
 ##############################
-. $(bits-include CMakeRecipe)
+. $(bits-include MakeRecipe)
 ##############################
 MODULE_OPTIONS="--bin --lib"
 ##############################
 function Make() {
-  make ${JOBS:+-j $JOBS} GSL_HOME=${GSL_ROOT}
-  cmake -E make_directory $INSTALLROOT/bin
-cmake
+  make ${JOBS:+-j $JOBS} GSL_HOME="${GSL_ROOT}"
+}
+
+function MakeInstall() {
+  install -dm755 "$INSTALLROOT/bin" "$INSTALLROOT/lib"
+  [ -f hto4l ] && install -m755 hto4l "$INSTALLROOT/bin/"
+  [ -f libHto4l.a ] && install -m644 libHto4l.a "$INSTALLROOT/lib/"
 }
--- pyjwt.sh
+++ pyjwt.sh
@@ -3,7 +3,7 @@ description: PyJWT JSON Web Token implementation for Python
 version: "2.8.0"
 tag: "2.8.0"
 sources:
-  - https://lcgpackages.web.cern.ch/tarFiles/sources/%(name)s-%(version)s.tar.gz
+  - https://files.pythonhosted.org/packages/source/P/PyJWT/%(name)s-%(version)s.tar.gz
 requires:
   - Python
 build_requires:
