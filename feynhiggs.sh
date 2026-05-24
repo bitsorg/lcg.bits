25a61f660252311efd6f5e5f085012382da373fd
Predrag Buncic <pbuncic@yahoo.com>
2026-05-24 12:17:27 +0200

    Fixing recipes

--- feynhiggs.sh
+++ feynhiggs.sh
@@ -19,9 +19,9 @@ MODULE_OPTIONS="--bin --lib"
 ##############################
 function Prepare() {
   rsync -av --delete --exclude '**/.git' --delete-excluded "${SOURCEDIR}"/ ./
-  # HiggsProd.F passes O'155' (octal BOZ) as a subroutine argument; GCC 15
-  # rejects this even with -fallow-invalid-boz.  Replace with the decimal value.
-  find . -name 'HiggsProd.F' -exec sed -i "s/O'155'/109/g" {} \;
+  # Several .F files pass O'155' (octal BOZ = 109 decimal) as subroutine arguments;
+  # GCC 15 rejects this even with -fallow-invalid-boz.  Replace with the decimal value.
+  find . -name '*.F' -exec sed -i "s/O'155'/109/g" {} \;
 }
 function Configure() {
   ./configure --prefix="$INSTALLROOT" F77=${FC:-gfortran} \
