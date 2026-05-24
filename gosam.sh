bb330f0a357897720bfc734197680f3be06e3323
Predrag Buncic <pbuncic@yahoo.com>
2026-05-24 15:42:39 +0200

    Fixing recipes

--- gosam.sh
+++ gosam.sh
@@ -2,6 +2,8 @@ package: gosam
 description: GoSam automated one-loop amplitude generator
 version: "2.1.2"
 tag: "2.1.2"
+sources:
+  - https://lcgpackages.web.cern.ch/tarFiles/sources/MCGeneratorsTarFiles/gosam-2.1.2.tar.gz
 requires:
   - Python
   - setuptools
@@ -16,7 +18,7 @@ license: GPL-2.0-or-later
 ---
 #!/bin/bash -e
 ##############################
-. $(bits-include PythonPipRecipe)
+. $(bits-include PythonRecipe)
 ##############################
 MODULE_OPTIONS="--bin --python"
 ##############################
