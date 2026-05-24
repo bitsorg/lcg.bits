36c919689aa44592d64b08577b647d7bbec18ba7
Predrag Buncic <pbuncic@yahoo.com>
2026-05-24 13:43:12 +0200

    Fixing recipes

--- fontconfig.sh
+++ fontconfig.sh
@@ -22,6 +22,15 @@ license: MIT
 MODULE_OPTIONS="--bin --lib --pkgconfig"
 ##############################
 function Configure() {
-  export PKG_CONFIG_PATH="${FREETYPE_ROOT}/lib/pkgconfig:${EXPAT_ROOT}/lib/pkgconfig:${UUID_ROOT}/lib/pkgconfig${PKG_CONFIG_PATH:+:${PKG_CONFIG_PATH}}"
+  # Build pkg-config search path from available dep roots; fall back to common
+  # system paths for packages that may be provided by the OS (e.g. freetype
+  # when bits is invoked with --disable=freetype).
+  local _pc=""
+  for _root in "${FREETYPE_ROOT}" "${EXPAT_ROOT}" "${UUID_ROOT}"; do
+    [ -n "${_root}" ] && _pc="${_pc:+${_pc}:}${_root}/lib/pkgconfig"
+  done
+  # Append system fallback paths for anything not found above.
+  _pc="${_pc:+${_pc}:}/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig"
+  export PKG_CONFIG_PATH="${_pc}${PKG_CONFIG_PATH:+:${PKG_CONFIG_PATH}}"
   ./configure --disable-docs --prefix="${INSTALLROOT}"
 }
