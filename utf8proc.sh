cd62571654020c85a45dec8d11e9f043f24a48ed
Predrag Buncic <pbuncic@yahoo.com>
2026-05-24 14:14:26 +0200

    Fixing recipes

--- utf8proc.sh
+++ utf8proc.sh
@@ -17,3 +17,8 @@ license: MIT
 ##############################
 MODULE_OPTIONS="--bin --lib --cmake"
 ##############################
+function Configure() {
+  cmake "${SOURCEDIR}" \
+    -DCMAKE_INSTALL_PREFIX="${INSTALLROOT}" \
+    -DBUILD_SHARED_LIBS=ON
+}
