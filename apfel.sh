492a8ba0f2acee2c51a826e801be839f1622ea34
Predrag Buncic <pbuncic@yahoo.com>
2026-05-25 14:10:06 +0200

    Fixing recipes

--- apfel.sh
+++ apfel.sh
@@ -5,7 +5,6 @@ tag: "3.0.4"
 sources:
   - https://lcgpackages.web.cern.ch/tarFiles/sources/MCGeneratorsTarFiles/%(name)s-%(version)s.tar.gz
 requires:
-  - CMake
   - lhapdf
   - swig
   - Python
@@ -16,18 +15,14 @@ license: GPL-3.0-or-later
 ---
 #!/bin/bash -e
 ##############################
-. $(bits-include CMakeRecipe)
+. $(bits-include AutoToolsRecipe)
 ##############################
 MODULE_OPTIONS="--bin --lib"
 ##############################
 function Configure() {
-  cmake "${SOURCEDIR}" \
-      -DCMAKE_INSTALL_PREFIX="${INSTALLROOT}" \
-    ${CMAKE_PREFIX_PATH:+-DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}"} \
-      -DCMAKE_BUILD_TYPE=Release \
-    -DAPFEL_ENABLE_PYTHON=ON \
-    -DAPFEL_ENABLE_TESTS=ON \
-    -DAPFEL_ENABLE_LHAPDF=ON \
-    -DAPFEL_DOWNLOAD_PDFS=ON \
-    -DAPFEL_Python_SITEARCH=autoprefix
+  [ -f autogen.sh ] && ./autogen.sh
+  ./configure --prefix="${INSTALLROOT}" \
+    --enable-python \
+    ${LHAPDF_ROOT:+--with-lhapdf="${LHAPDF_ROOT}"} \
+    ${PYTHON_EXE:+PYTHON="${PYTHON_EXE}"}
 }
