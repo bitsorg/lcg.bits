55d8b0b5280fcb63ce9c066f536cf5b6d4ed83df
Predrag Buncic <pbuncic@yahoo.com>
2026-05-26 13:13:03 +0200

    Fixing recipes

--- hyperopt.sh
+++ hyperopt.sh
@@ -1,17 +1,9 @@
 package: hyperopt
 description: Hyperopt distributed hyperparameter optimization
-version: "0.2.8"
-tag: "0.2.8"
+version: "0.2.7"
+tag: "0.2.7"
 requires:
   - Python
-  - cloudpickle
-  - networkx
-  - numpy
-  - py4j
-  - scipy
-  - six
-  - tqdm
-  - arrow
 build_requires:
   - bits-recipe-tools
   - "GCC-Toolchain:(?!osx)"
--- pycairo.sh
+++ pycairo.sh
@@ -1,9 +1,9 @@
 package: pycairo
 description: Pycairo Python bindings for the Cairo library
-version: "1.28.0"
-tag: "1.28.0"
-sources:
-  - https://lcgpackages.web.cern.ch/tarFiles/sources/%(name)s-%(version)s.tar.gz
+version: "1.29.0"
+tag: "1.29.0"
+#sources:
+#  - https://lcgpackages.web.cern.ch/tarFiles/sources/%(name)s-%(version)s.tar.gz
 requires:
   - Python
   - cairo
@@ -16,7 +16,7 @@ license: LGPL-2.1-only OR MPL-1.1
 export PKG_CONFIG_PATH="/usr/share/pkgconfig:${PKG_CONFIG_PATH:-}"
 [ "$(uname -s)" = "Darwin" ] && export CPPFLAGS="-I/opt/X11/include ${CPPFLAGS:-}"
 ##############################
-. $(bits-include PythonRecipe)
+. $(bits-include PythonPipRecipe)
 ##############################
 MODULE_OPTIONS="--bin --python"
 ##############################
