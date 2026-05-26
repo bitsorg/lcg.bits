eae8abc2fe0593a8437d57d413e78aecf440b545
Predrag Buncic <pbuncic@yahoo.com>
2026-05-26 13:17:58 +0200

    Fixing recipes

--- harfbuzz.sh
+++ harfbuzz.sh
@@ -24,7 +24,9 @@ MODULE_OPTIONS="--bin --lib --pkgconfig"
 ##############################
 function Configure() {
   # configure uses pkg-config to locate glib-2.0, freetype2, cairo, etc.
-  # All non-system deps must be on PKG_CONFIG_PATH so the checks pass.
+  # All non-system dep lib dirs must be in PKG_CONFIG_PATH (so pkg-config
+  # finds them) and LD_LIBRARY_PATH (so the configure compile-and-link tests
+  # can actually run the produced binaries against the shared libs).
   export PKG_CONFIG_PATH="\
 ${GLIB_ROOT:+${GLIB_ROOT}/lib/pkgconfig}\
 ${FREETYPE_ROOT:+:${FREETYPE_ROOT}/lib/pkgconfig}\
@@ -32,5 +34,12 @@ ${CAIRO_ROOT:+:${CAIRO_ROOT}/lib/pkgconfig}\
 ${LIBFFI_ROOT:+:${LIBFFI_ROOT}/lib/pkgconfig}\
 ${GETTEXT_ROOT:+:${GETTEXT_ROOT}/lib/pkgconfig}\
 ${PKG_CONFIG_PATH:+:${PKG_CONFIG_PATH}}"
+  export LD_LIBRARY_PATH="\
+${GLIB_ROOT:+${GLIB_ROOT}/lib}\
+${FREETYPE_ROOT:+:${FREETYPE_ROOT}/lib}\
+${CAIRO_ROOT:+:${CAIRO_ROOT}/lib}\
+${LIBFFI_ROOT:+:${LIBFFI_ROOT}/lib}\
+${GETTEXT_ROOT:+:${GETTEXT_ROOT}/lib}\
+${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
   ./configure --prefix="${INSTALLROOT}" --with-cairo --with-freetype --with-glib
 }
