119f1ee14d8109fb8c87b8c3fa916751ec21a54b
Predrag Buncic <pbuncic@yahoo.com>
2026-06-03 11:19:20 +0200

    Adding minimal defaults-release.sh

---
+++ defaults-release.sh
@@ -0,0 +1,9 @@
+package: defaults-release
+version: v1
+env:
+  CXXFLAGS: "-fPIC -g -O2 -std=c++20"
+  CFLAGS: "-fPIC -g -O2"
+  CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+  MACOSX_DEPLOYMENT_TARGET: '14.0'
+---
+~    
